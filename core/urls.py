c1ccabff9e1238b6b8af0630c4ca0d7dc4515876
Kyaw Soe Hla <kyawsoehla@gmail.com>
2022-07-23 10:12:37 -0700

    Show All Posts and base file and category file are added

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('post/<str:slug>/', views.detail, name="detail"),
+    path('blog/', views.allBlog, name="allblog"),
+    path('category/<str:slug>/', views.categorypost, name="categorypost"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import About, Blog, Contact, Gallery, Hero, Skill, Year
+from .models import About, Blog, Category, Contact, Gallery, Hero, Skill, Year
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 def home(request):
     about = About.objects.first()
@@ -18,3 +21,21 @@ def detail(request,slug):
 
     context = {'blog':blog}
     return render(request,'layouts/detail.html',context)
+
+def allBlog(request):
+    about = About.objects.first()
+    contact = Contact.objects.first()
+    hero = Hero.objects.first()
+    categories = Category.objects.all().annotate(posts_count=Count('blog'))
+    blogs = Blog.objects.all()
+    gallery = Gallery.objects.all()
+    years = Year.objects.all()
+    context = {"about":about,'contact':contact,'categories':categories,'hero':hero,'blogs':blogs,'gallery':gallery,'years':years}
+    return render(request,'layouts/allblogs.html',context)
+
+def categorypost(request,slug):
+    category = get_object_or_404(Category, slug=slug)
+    blogs = Blog.objects.filter(category=category)
+    categories = Category.objects.all().annotate(posts_count=Count('blog'))
+    context = {'categories':categories,'blogs':blogs,'slug':slug}
+    return render(request,'layouts/categorypost.html',context)
